e138c0b3161e272e661fbe536b176105bfcf1843
Amer <amermmhamad@gmail.com>
2025-09-14 14:00:42 +0300

    Fixed

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -39,8 +39,11 @@ class Teacher implements TeacherInterface {
 }
 
 function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === "number" && salary < 500) {
-    return new Teacher();
+  if (typeof salary === "number") {
+    if (salary < 500) {
+      return new Teacher();
+    }
+    return new Director();
   }
   return new Director();
 }
